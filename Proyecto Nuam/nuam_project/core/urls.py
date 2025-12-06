738819d8fa051fae998bf86f1942e239392642ae
Claudio Fabian Andres Pérez Alcayaga <claudio.perez87@inacapmail.cl>
2025-12-06 13:52:02 -0300

    feat: Agregar ruta para carga masiva y actualizar archivos de configuración

--- Proyecto Nuam/nuam_project/core/urls.py
+++ Proyecto Nuam/nuam_project/core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # Rutas básicas de autenticación (para simplificar, usaremos las que trae Django)
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('carga-masiva/', views.carga_masiva_view, name='carga_masiva'),
 ]
