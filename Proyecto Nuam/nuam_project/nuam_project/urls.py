44f4a9182683ab645e4ba27277033d92886d9d74
Claudio Fabian Andres Pérez Alcayaga <claudio.perez87@inacapmail.cl>
2025-12-06 12:52:35 -0300

    Actualizar configuración de URLs para incluir redirección a la página de login y agregar rutas de la app 'core'

--- Proyecto Nuam/nuam_project/nuam_project/urls.py
+++ Proyecto Nuam/nuam_project/nuam_project/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for nuam_project project.
+# nuam_project/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.views import mantenedor_redirect # Importación nueva
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 1. Incluir las URLs de la app 'core'
+    path('', include('core.urls')),
+    
+    # 2. Redirigir la raíz del sitio (/) a la página de login o al mantenedor
+    path('', mantenedor_redirect, name='home'), 
 ]
